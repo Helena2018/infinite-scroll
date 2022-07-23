9ea8f8145b1d3b9d9674f16b4f0483a69c57ae6c
Helena2018 <helena2020liu@gmail.com>
2022-07-23 18:13:55 +1200

    create <a> to link Unsplash

--- script.js
+++ script.js
@@ -8,12 +8,23 @@ const count = 10;
 const apiKey = 'ctMG8haf-qRPxQBBq7TUoMjFlSsyVm8D_PWD0iPA718'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
 
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    item.setAttribute('href', photo.link.html)
+    item.setAttribute('target', '_blank')
+  })
+}
+
 // Get photos from Unsplash API
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
     photosArray = response.json();
-    console.log(photosArray);
+    displayPhotos();
   } catch(error) {
     // Catch Error Here
   }
