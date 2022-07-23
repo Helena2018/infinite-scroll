db2b10cbd16a38671b4ebc2e1e53a79866e2f68e
Helena2018 <helena2020liu@gmail.com>
2022-07-23 17:38:04 +1200

    get photos from Unsplash API

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-//  Unsplash API
+// Unsplash API
 const count = 10;
-const apiKey = ''
+const apiKey = 'ctMG8haf-qRPxQBBq7TUoMjFlSsyVm8D_PWD0iPA718'
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`
+
+// Get photos from Unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = response.json();
+    console.log(data);
+  } catch(error) {
+    // Catch Error Here
+  }
+}
+
+// On Load
+getPhotos()
